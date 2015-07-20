27bc45a0b31e1be390721ac3ed0b1f3ff536e801
Brian George <brian@pelotoncycle.com>
2015-07-20 15:11:36 -0400

    lint and remove browser.name from directory

--- index.js
+++ index.js
@@ -1,147 +1,217 @@
-var os = require('os')
-var path = require('path')
-var fs = require('fs')
-var builder = require('xmlbuilder')
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
+var builder = require('xmlbuilder');
 
-var JUnitReporter = function (baseReporterDecorator, config, logger, helper, formatError) {
-  var log = logger.create('reporter.junit')
-  var reporterConfig = config.junitReporter || {}
-  var pkgName = reporterConfig.suite || ''
-  var outputFileName = reporterConfig.outputFile
+var JUnitReporter = function(baseReporterDecorator, config, logger,
+    helper, formatError) {
 
-  if (!reporterConfig.outputDir) {
-    throw new Error('You must set an output directory for JUnitReporter via the outputDir config property')
-  }
+    var log = logger.create('reporter.junit');
+    var reporterConfig = config.junitReporter || {};
+    var pkgName = reporterConfig.suite || '';
+    var outputFileName = reporterConfig.outputFile;
 
-  var outputDir = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputDir)) + path.sep
-  var suites
-  var pendingFileWritings = 0
-  var fileWritingFinished = function () {}
-  var allMessages = []
+    if (!reporterConfig.outputDir) {
 
-  baseReporterDecorator(this)
+        throw new Error('You must set an output directory for ' +
+            'JUnitReporter via the outputDir config property');
 
-  this.adapters = [function (msg) {
-    allMessages.push(msg)
-  }]
-
-  var initliazeXmlForBrowser = function (browser) {
-    var timestamp = (new Date()).toISOString().substr(0, 19)
-    var suite = suites[browser.id] = builder.create('testsuite')
-    suite.att('name', browser.name)
-      .att('package', pkgName)
-      .att('timestamp', timestamp)
-      .att('id', 0)
-      .att('hostname', os.hostname())
-    suite.ele('properties')
-      .ele('property', {name: 'browser.fullName', value: browser.fullName})
-  }
-
-  var writeXmlForBrowser = function (browser) {
-    var outputFile = path.join(outputDir, browser.name.replace(/ /g, '_'))
-    var xmlToOutput = suites[browser.id]
-
-    if (outputFileName) {
-      outputFile = path.join(outputFile, outputFileName)
-    } else {
-      outputFile += '.xml'
     }
 
-    pendingFileWritings++
-    helper.mkdirIfNotExists(outputDir, function () {
-      fs.writeFile(outputFile, xmlToOutput.end({pretty: true}), function (err) {
-        if (err) {
-          log.warn('Cannot write JUnit xml\n\t' + err.message)
-        } else {
-          log.debug('JUnit results written to "%s".', outputFile)
+    var outputDir = helper.normalizeWinPath(
+        path.resolve(config.basePath, reporterConfig.outputDir)) + path.sep;
+    var suites;
+    var pendingFileWritings = 0;
+    var fileWritingFinished = function() {};
+    var allMessages = [];
+
+    baseReporterDecorator(this);
+
+    this.adapters = [function(msg) {
+
+        allMessages.push(msg);
+
+    }];
+
+    var initializeXmlForBrowser = function(browser) {
+
+        var timestamp = (new Date()).toISOString().substr(0, 19);
+        var suite = suites[browser.id] = builder.create('testsuite');
+        suite.att('name', browser.name)
+            .att('package', pkgName)
+            .att('timestamp', timestamp)
+            .att('id', 0)
+            .att('hostname', os.hostname());
+        suite.ele('properties')
+                .ele('property', {
+                    name: 'browser.fullName', value: browser.fullName
+                });
+
+    };
+
+    var writeXmlForBrowser = function(browser) {
+
+        var outputFile = path.join(outputDir);
+        var xmlToOutput = suites[browser.id];
+
+        if (outputFileName) {
+
+            outputFile = path.join(outputFile, outputFileName);
+
+        }
+        else {
+
+            outputFile += '.xml';
+
         }
 
-        if (!--pendingFileWritings) {
-          fileWritingFinished()
+        pendingFileWritings++;
+        helper.mkdirIfNotExists(outputDir, function() {
+
+            fs.writeFile(outputFile, xmlToOutput.end({
+                pretty: true
+            }), function(err) {
+
+                if (err) {
+
+                    log.warn('Cannot write JUnit xml\n\t' + err.message);
+
+                }
+                else {
+
+                    log.debug('JUnit results written to "%s".', outputFile);
+
+                }
+
+                if (!--pendingFileWritings) {
+
+                    fileWritingFinished();
+
+                }
+
+            });
+
+        });
+
+    };
+
+    this.onRunStart = function(browsers) {
+
+        suites = Object.create(null);
+
+        // TODO(vojta): remove once we don't care about Karma 0.10
+        browsers.forEach(initializeXmlForBrowser);
+
+    };
+
+    this.onBrowserStart = function(browser) {
+
+        initializeXmlForBrowser(browser);
+
+    };
+
+    this.onBrowserComplete = function(browser) {
+
+        var suite = suites[browser.id];
+
+        if (!suite) {
+
+            // This browser did not signal `onBrowserStart`. That happens
+            // if the browser timed out during the start phase or javascript
+            // exception has occured.
+            initializeXmlForBrowser(browser);
+            if (browser.lastResult.error) {
+
+                suite = suites[browser.id];
+                suite.ele('error').dat(allMessages.join() + '\n');
+                allMessages = [];
+
+            }
+
         }
-      })
-    })
-  }
+        else {
 
-  this.onRunStart = function (browsers) {
-    suites = Object.create(null)
+            var result = browser.lastResult;
 
-    // TODO(vojta): remove once we don't care about Karma 0.10
-    browsers.forEach(initliazeXmlForBrowser)
-  }
+            suite.att('tests', result.total);
+            suite.att('errors', result.disconnected || result.error ? 1 : 0);
+            suite.att('failures', result.failed);
+            suite.att('time', (result.netTime || 0) / 1000);
 
-  this.onBrowserStart = function (browser) {
-    initliazeXmlForBrowser(browser)
-  }
+            if (result.disconnected) {
 
-  this.onBrowserComplete = function (browser) {
-    var suite = suites[browser.id]
+                suite.ele('error').att('message', 'Browser disconnected');
 
-    if (!suite) {
-      // This browser did not signal `onBrowserStart`. That happens
-      // if the browser timed out during the start phase or javascript
-      // exception has occured.
-      initliazeXmlForBrowser(browser)
-      if (browser.lastResult.error) {
-        suite = suites[browser.id]
-        suite.ele('error').dat(allMessages.join() + '\n')
-        allMessages = []
-      }
-    } else {
-      var result = browser.lastResult
+            }
 
-      suite.att('tests', result.total)
-      suite.att('errors', result.disconnected || result.error ? 1 : 0)
-      suite.att('failures', result.failed)
-      suite.att('time', (result.netTime || 0) / 1000)
+            suite.ele('system-out').dat(allMessages.join() + '\n');
+            allMessages = [];
+            suite.ele('system-err');
 
-      if (result.disconnected) {
-        suite.ele('error').att('message', 'Browser disconnected')
-      }
+        }
 
-      suite.ele('system-out').dat(allMessages.join() + '\n')
-      allMessages = []
-      suite.ele('system-err')
-    }
+        writeXmlForBrowser(browser);
 
-    writeXmlForBrowser(browser)
-  }
+    };
 
-  this.onRunComplete = function () {
-    suites = null
-    allMessages.length = 0
-  }
+    this.onRunComplete = function() {
 
-  this.specSuccess = this.specSkipped = this.specFailure = function (browser, result) {
-    var spec = suites[browser.id].ele('testcase', {
-      name: result.description, time: ((result.time || 0) / 1000),
-      classname: (pkgName ? pkgName + ' ' : '') + result.suite.join(' ').replace(/\./g, '_')
-    })
+        suites = null;
+        allMessages.length = 0;
 
-    if (result.skipped) {
-      spec.ele('skipped')
-    }
+    };
 
-    if (!result.success) {
-      result.log.forEach(function (err) {
-        spec.ele('failure', {type: ''}, formatError(err))
-      })
-    }
-  }
+    this.specSuccess = this.specSkipped = this.specFailure =
+            function(browser, result) {
 
-  // wait for writing all the xml files, before exiting
-  this.onExit = function (done) {
-    if (pendingFileWritings) {
-      fileWritingFinished = done
-    } else {
-      done()
-    }
-  }
-}
+                var spec = suites[browser.id].ele('testcase', {
+                    name: result.description, time: ((result.time || 0) / 1000),
+                    classname: (pkgName ? pkgName + ' ' : '') +
+                        result.suite.join(' ').replace(/\./g, '_')
+                });
 
-JUnitReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper', 'formatError']
+                if (result.skipped) {
+
+                    spec.ele('skipped');
+
+                }
+
+                if (!result.success) {
+
+                    result.log.forEach(function(err) {
+
+                        spec.ele('failure', {
+                            type: ''
+                        }, formatError(err));
+
+                    });
+
+                }
+
+            };
+
+    // wait for writing all the xml files, before exiting
+    this.onExit = function(done) {
+
+        if (pendingFileWritings) {
+
+            fileWritingFinished = done;
+
+        }
+        else {
+
+            done();
+
+        }
+
+    };
+
+};
+
+JUnitReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper',
+    'formatError'];
 
 // PUBLISH DI MODULE
 module.exports = {
-  'reporter:junit': ['type', JUnitReporter]
-}
+    'reporter:junit': ['type', JUnitReporter]
+};
